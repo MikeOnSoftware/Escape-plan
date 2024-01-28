4391dbe13d355065ee8200ba4da3bc804102e3e7
Mihail Rusanov <rusanov.mihail@gmail.com>
2024-01-28 17:19:55 +0200

    Fix particles

---
+++ Bullet.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float bulletSpeed = 12f;
+
+    [SerializeField] ParticleSystem bulletSmoke;
+
+    Rigidbody2D myRigidbody;
+    Player      player;
+    float       xSpeed;
+
+    void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<Player>();
+        xSpeed = player.transform.localScale.x * bulletSpeed;
+
+        var bulletRot = bulletSmoke.transform.rotation;
+        if (xSpeed < 0) bulletSmoke.transform.rotation = new Quaternion(bulletRot.x, bulletRot.y + 180, bulletRot.z, bulletRot.w);
+    }
+
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2 (xSpeed, 0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Buba"))
+        {
+            Destroy(other.gameObject, .2f);
+        }
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Destroy(gameObject);    
+    }
+
+}
