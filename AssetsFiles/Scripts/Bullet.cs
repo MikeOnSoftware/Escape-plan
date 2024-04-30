f9cf0aaa504102dcb8d4e84de8d485f0e3e152bd
Mihail Rusanov <rusanov.mihail@gmail.com>
2024-04-30 12:49:48 +0300

    Refactor Bullet.cs

--- AssetsFiles/Scripts/Bullet.cs
+++ AssetsFiles/Scripts/Bullet.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -18,27 +16,26 @@ public class Bullet : MonoBehaviour
         player = FindObjectOfType<Player>();
         xSpeed = player.transform.localScale.x * bulletSpeed;
 
-        var bulletRot = bulletSmoke.transform.rotation;
-        if (xSpeed < 0) bulletSmoke.transform.rotation = new Quaternion(bulletRot.x, bulletRot.y + 180, bulletRot.z, bulletRot.w);
+        SetSmokeRotEqualsBulletRot();
     }
 
-    void Update()
+    void SetSmokeRotEqualsBulletRot()
     {
-        myRigidbody.velocity = new Vector2 (xSpeed, 0f);
+        var bulletRot = bulletSmoke.transform.rotation;
+        if (xSpeed < 0)
+            bulletSmoke.transform.rotation = new Quaternion(bulletRot.x, bulletRot.y + 180, bulletRot.z, bulletRot.w);
     }
+
+    void Update() => myRigidbody.velocity = new Vector2 (xSpeed, 0f);
 
     void OnTriggerEnter2D(Collider2D other) 
     {
-        if(other.CompareTag("Buba"))
+        if (other.CompareTag("Buba"))
         {
             Destroy(other.gameObject, .2f);
         }
         Destroy(gameObject);
     }
 
-    void OnCollisionEnter2D(Collision2D other) 
-    {
-        Destroy(gameObject);    
-    }
-
+    void OnCollisionEnter2D(Collision2D other) => Destroy(gameObject);
 }
